5bb281b4458d7da464e839da324bbb4d3400d896
Vo Van Tri <vo.van.tri@framgia.com>
2018-01-15 09:07:20 +0700

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def full_title page_title = ""
+  
+    if page_title.empty?
+      t "layouts.title"
+    else
+      page_title + " | " + t("layouts.title")
+    end
+  end
+
 end
